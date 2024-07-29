867d849a2f10bbf9be025bccdf624f142e6bd392
Omar Aglan <omar.aglan91@yahoo.com>
2024-07-29 03:26:56 +0300

    Massive Update On hero Sections

--- front_end/components/Hero.tsx
+++ front_end/components/Hero.tsx
@@ -7,36 +7,38 @@ const Hero = () =>{
 
     }
     return (
-        <div className="hero">
-            <div className="flex-1 pt-36 padding-x">
-                <h1 className="hero__title">
-                    الوسيط الرقمي الأول
-                    لتمويل السيارات بالمملكة العربية السعودية
+        <main className=" bg-white relative overflow-hidden">
+            <div className="bg-white  flex relative z-20 items-center overflow-hidden">
+        <div className="container mx-auto px-6 flex relative py-16">
+            <div className="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
+                <span className="w-20 h-2 bg-gray-800 mb-12">
+                </span>
+                <h1 className="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none  text-gray-800">
+                Syaarty
+                    <span className="text-5xl sm:text-7xl">
+                        سيارتي
+                    </span>
                 </h1>
-
-                <p className="hero__subtitle">
-                    منصتك الأولى للوساطة الرقمية في تمويل السيارات داخل المملكة احصل على سيارتك بنقرة لباب بيتك
+                <br />
+                <span className="w-20 h-2 bg-primary-blue mt-8"></span>
+                <p className="text-base sm:text-base text-gray-700">
+                الوسيط الرقمي الأول لتمويل السيارات بالمملكة العربية السعودية.
                 </p>
-
-                <CustomButton
-                    title={"طلب تمويل"}
-                    containerStyles={"bg-primary-blue rounded-full mt-10 text-white"}
-                    handleClick={handleMoving}
-                />
+                <div className="flex mt-8">
+                    <a href="#" className="uppercase py-2 px-4 rounded-lg bg-primary-blue border-2 border-transparent text-white text-md mr-4 hover:bg-slate-900">
+                        أطلب تمويل
+                    </a>
+                    <a href="#" className="uppercase py-2 px-4 rounded-lg bg-transparent border-2 bg-primary-blue text-blacksection  hover:bg-slate-900 hover:text-white text-md">
+                        ابحث عن السيارات
+                    </a>
+                </div>
             </div>
-            <div className="hero__image-container">
-                <div className="hero__image">
-                    <Image
-                    src="/2020_Altima_Big.png"
-                    alt={"Hero"}
-                    fill
-                    className="object-contain"
-                    />
-                    <div className="hero__image-overlay" />
-                <div/>
+            <div className="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
+                <img src="/2020_Altima_Big.png" className="max-w-xs md:max-w-sm m-auto"/>
             </div>
         </div>
-        </div>
+    </div>
+        </main>
     )
 }
 
--- front_end/components/NavBar.tsx
+++ front_end/components/NavBar.tsx
@@ -1,55 +1,150 @@
-"use client"
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-import CustomButton from "./CustomButton";
+'use client';
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 
-const NavBar = () => {
-    const [navigationOpen, setNavigationOpen] = useState(false);
-    const [dropdownToggler, setDropdownToggler] = useState(false);
-    const [stickyMenu, setStickyMenu] = useState(false);
-    
+interface NavBarProps {
+    navigation: { name: string, href: string, current: boolean }[];
+}
+const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+  { name: 'الرئيسية', href: '/', current: true },
+  { name: 'سيارات جديدة', href: '/search', current: false },
+  { name: 'المعارض', href: '#', current: false },
+  { name: 'المدونة', href: '#', current: false },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
 
-    const pathname = usePathname();
-
-    //sticky menu
-    const handleStickyMenu = () => {
-        if (window.scrollY >= 80) {
-            setStickyMenu(true);
-        }
-        else {
-            setStickyMenu(false);
-        }
-    }
-
-    useEffect(() => {
-        window.addEventListener("scroll", handleStickyMenu);
-    })
-    return (
-        <header className="w-full absolute z-10">
-            <nav className="max-w-[1440px] mx-auto flex justify-between
-             items-center sm:px-16 px-6 py-4">
-                <Link href="/" className="flex justify-center items-center">
-                <Image
-                 src="/logo.png"
-                 alt="سيارتي"
-                 width={125}
-                 height={20}
-                className="object-contain"
-                 />
-                </Link>
-
-
-                <CustomButton
-                title = "طلب تمويل"
-                btnType = "button"
-                containerStyles = "text-primary-blue rounded-full bg-blue-300 min-w-[130px]"
-                handleClick = {() => {}}
-                />
-            </nav>
-        </header>
-    )
+function classNames(...classes: (string | number)[]) {
+  return classes.filter(Boolean).join(' ');
 }
 
-export default NavBar
+export default function Example() {
+    const pathname = usePathname()
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-primary-blue">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <div className="flex-1">
+                  <img
+                    alt="سيارتي"
+                    src="/logo.png"
+                    className="h-11 w-11"
+                  />
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        aria-current={item.current ? 'page' : undefined}
+                        className={clsx(
+                            'text-slate-950 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium',
+                            {
+                              'bg-sky-100 text-cyan-950': pathname === item.href,
+                            },
+                          )}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-4 flex items-center md:ml-6">
+
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                    <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">طلب تمويل</button>
+                    </div>
+                    
+                  </Menu>
+                </div>
+              </div>
+              <div className="-mr-2 flex md:hidden">
+                {/* Mobile menu button */}
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+                  <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+                </DisclosureButton>
+              </div>
+            </div>
+          </div>
+
+          <DisclosurePanel className="md:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+              {navigation.map((item) => (
+                <DisclosureButton
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  aria-current={item.current ? 'page' : undefined}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium',
+                  )}
+                >
+                  {item.name}
+                </DisclosureButton>
+              ))}
+            </div>
+            <div className="border-t border-gray-700 pb-3 pt-4">
+              <div className="flex items-center px-5">
+                <div className="flex-shrink-0">
+                  <img alt="" src={user.imageUrl} className="h-10 w-10 rounded-full" />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium leading-none text-white">{user.name}</div>
+                  <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                </div>
+                
+              </div>
+              <div className="mt-3 space-y-1 px-2">
+                {userNavigation.map((item) => (
+                  <DisclosureButton
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                  >
+                    {item.name}
+                  </DisclosureButton>
+                ))}
+              </div>
+            </div>
+          </DisclosurePanel>
+        </Disclosure>  
+        <main>
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+        </main>
+      </div>
+    </>
+  )
+}
